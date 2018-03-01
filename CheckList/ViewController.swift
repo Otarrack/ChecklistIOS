5e143b2650749319e21ddcc510dfdee4320fc28e
Florian Van Den Berghe <vandenber.florian@gmail.com>
2018-03-01 17:00:31 +0100

    Initial Commit

---
+++ CheckList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Florian Van Den Berghe on 01/03/2018.
+//  Copyright © 2018 Florian Van Den Berghe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
