1c1771b1e5209e3f7a6cb3628e20562a53d8cad8
Florian Van Den Berghe <vandenber.florian@gmail.com>
2018-03-08 17:40:50 +0100

    first commit

---
+++ CheckList/CheckListItem.swift
@@ -0,0 +1,9 @@
+//
+//  CheckListItem.swift
+//  CheckList
+//
+//  Created by Florian Van Den Berghe on 08/03/2018.
+//  Copyright © 2018 Florian Van Den Berghe. All rights reserved.
+//
+
+import Foundation
